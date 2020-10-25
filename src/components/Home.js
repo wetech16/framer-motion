298fcf228565704f717678e6a0f1eb6e92570866
Stephen Guo <wetech16@gmail.com>
2020-10-25 12:16:17 -0700

    -add files master

---
+++ src/components/Home.js
@@ -0,0 +1,37 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import { motion } from 'framer-motion';
+
+const buttonVariants = {
+  hover: {
+    scale: 1.1,
+    textShadow: "0px 0px 8px rgb(255,255,255)",
+    boxShadow: "0px 0px 8px rgb(255,255,255)",
+    transition: {
+      duration: 0.3,
+      yoyo: 5
+    }
+  }
+}
+
+const Home = () => {
+  return (
+    <motion.div className="home container"
+      initial={{ opacity: 0 }}
+      animate={{ opacity: 1 }}
+      transition={{ delay: 1.5, duration: 1.5 }}
+    >
+      <h2>Welcome to Pizza Joint</h2>
+      <Link to="/base">
+        <motion.button
+          variants={buttonVariants}
+          whileHover="hover"
+        >
+          Create Your Pizza
+        </motion.button>
+      </Link>
+    </motion.div>
+  )
+}
+
+export default Home;
